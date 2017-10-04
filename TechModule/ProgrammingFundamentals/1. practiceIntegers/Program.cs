75ef18f00c6dbcff1ea60d00eb0fbaec5e48b421
Bogdan Grozdev <grozdevbogdan@gmail.com>
2017-10-04 05:46:17 +0300

    Added homework

---
+++ TechModule/ProgrammingFundamentals/1. practiceIntegers/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1.practiceIntegers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            sbyte a = sbyte.Parse(Console.ReadLine());
+            byte b = byte.Parse(Console.ReadLine());
+            short c = short.Parse(Console.ReadLine());
+            ushort d = ushort.Parse(Console.ReadLine());
+            uint e = uint.Parse(Console.ReadLine());
+            int f = int.Parse(Console.ReadLine());
+            long g = long.Parse(Console.ReadLine());
+            sbyte num1 = a;
+            byte num2 = b;
+            short num3 = c;
+            ushort num4 = d;
+            uint num5 = e;
+            int num6 = f;
+            long num7 = g;
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.WriteLine(c);
+            Console.WriteLine(d);
+            Console.WriteLine(e);
+            Console.WriteLine(f);
+            Console.WriteLine(g);
+        }
+    }
+}
---
+++ TechModule/ProgrammingFundamentals/19. theaThePhotographer/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _19.theaThePhotographer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            long n = long.Parse(Console.ReadLine());
+            long filterTime = long.Parse(Console.ReadLine());
+            long filterFactor = long.Parse(Console.ReadLine());
+            long uploadTime = long.Parse(Console.ReadLine());
+
+            long totalFilteringTime = n * filterTime;
+            long goodPictures = (long)(Math.Ceiling(n * filterFactor / 100d));
+            long totalUploadTime = goodPictures * uploadTime;
+
+            long totalTime = totalFilteringTime + totalUploadTime;
+
+            TimeSpan projectTime = TimeSpan.FromSeconds(totalTime);
+
+            Console.WriteLine("{0:D1}:{1:D2}:{2:D2}:{3:D2}",
+                projectTime.Days,
+                projectTime.Hours,
+                projectTime.Minutes,
+                projectTime.Seconds);
+
+
+        }
+    }
+}
